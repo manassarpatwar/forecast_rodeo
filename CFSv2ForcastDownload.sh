81539ecc129fae67a8099c98365fa13455ea18dc
Manas Sarpatwar <aca18mms@sharc-login1.shef.ac.uk>
2021-03-04 07:43:25 +0000

    Rodeo dataset download scripts

---
+++ CFSv2ForcastDownload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+token="bc50a084-bcd9-4ae1-a2c6-abf11d5f2c06" # http://guides.dataverse.org/en/latest/user/account.html#api-token
+output="/data/aca18mms/forecast_rodeo/forecasts/cfsv2_2011-2018/"
+curl -H "X-Dataverse-key: $token" https://dataverse.harvard.edu/api/datasets/:persistentId/versions/2/files?persistentId=doi:10.7910/DVN/CEFZLV | tee >(./jq '.data[].dataFile.persistentId' >persistentid) >(./jq '.data[].dataFile.filename' >filenames)
+paste persistentid filenames > arg
+cat arg | xargs -L1 bash -c ' wget -O '$output'$1 -P '$output' https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=$0'
+rm filenames persistentid arg
